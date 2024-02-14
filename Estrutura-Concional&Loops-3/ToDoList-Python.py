555ca1cec7c633e539b08406f6715371280bc293
Edu1Sinico <EduardoSenai888@gmail.com>
2024-02-14 17:00:23 -0300

    Enviando o projeto de Python para o github

---
+++ Estrutura-Concional&Loops-3/ToDoList-Python.py
@@ -0,0 +1,13 @@
+x = 0
+
+while x != 6 :
+    print("\n=== To-Do List ===")
+    print("1. Adicionar Atividade")
+    print("2. Remover Atividade")
+    print("3. Completar Atividade")
+    print("4. Editar Atividade")
+    print("5. Excluir Atividade")
+    print("6. Sair\n")
+
+    x = input()
+    print(x)
